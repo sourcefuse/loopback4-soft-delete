b31961980fe0a0b9f26c1a1187500e70d552762d
Samarpan <samarpan.bhattacharya@sourcefuse.com>
2019-06-11 17:51:23 +0530

    fix prettier issues

--- src/repositories/soft-crud.repository.base.ts
+++ src/repositories/soft-crud.repository.base.ts
@@ -6,16 +6,16 @@ import {
   juggler,
   Where,
 } from '@loopback/repository';
-import { Count } from '@loopback/repository/src/common-types';
-import { Options } from 'loopback-datasource-juggler';
+import {Count} from '@loopback/repository/src/common-types';
+import {Options} from 'loopback-datasource-juggler';
 
-import { SoftDeleteEntity } from '../models';
+import {SoftDeleteEntity} from '../models';
 
 export abstract class SoftCrudRepository<
   T extends SoftDeleteEntity,
   ID,
   Relations extends object = {}
-  > extends DefaultCrudRepository<T, ID, Relations> {
+> extends DefaultCrudRepository<T, ID, Relations> {
   constructor(
     entityClass: typeof SoftDeleteEntity & {
       prototype: T;
@@ -45,7 +45,11 @@ export abstract class SoftCrudRepository<
     return super.findOne(filter, options);
   }
 
-  findById(id: ID, filter?: Filter<T>, options?: Options): Promise<(T & Relations)> {
+  findById(
+    id: ID,
+    filter?: Filter<T>,
+    options?: Options,
+  ): Promise<T & Relations> {
     // Filter out soft deleted entries
     filter = filter || {};
     filter.where = filter.where || {};
