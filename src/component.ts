17c1df6effca50f4d4c66e2d5c2f15d65357f4c9
arpit1503khanna <arpit.khanna@sourcefuse.com>
2024-03-12 16:00:34 +0530

    feat(deps): dummy commit for major version release
    dummy commit for major version release
    
    BREAKING CHANGE:
    dummy commit for major version release

--- src/component.ts
+++ src/component.ts
@@ -3,7 +3,7 @@ export class SoftDeleteComponent implements Component {
   providers?: ProviderMap = {};
 
   constructor() {
-    // Initialize the providers property in the constructor
+    // Initialize providers property in the constructor
     this.providers = {};
   }
 }
